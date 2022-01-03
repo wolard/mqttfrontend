4e2ade18a2a9c9b0122eeea151918b98578e9107
Klaus Sandell <wolard@gmail.com>
2022-01-03 15:31:54 +0200

    added websocket and basic canvas leds

--- src/App.js
+++ src/App.js
@@ -1,31 +1,104 @@
-import { RgbaColorPicker } from "react-colorful";
-import {useState} from 'react'
+import { useState, useEffect, } from 'react';
+import { Circle, Layer, Stage } from 'react-konva';
+import socketIOClient from "socket.io-client";
+let socket;
+const initiateSocketConnection =  () => {
+  socket = socketIOClient('http://localhost:3005');
+  console.log(`Connecting socket...`);
+  console.log(socket)
+ }
 
- 
 
-function App() {
-  
-  const handleSetColor=   async() =>
-{
-  console.log(color)
-  const requestOptions = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json'},
-    body: JSON.stringify(color)
-};
- await fetch('http://192.168.1.201:3005/setleds', requestOptions)
-    .then (response=> {
-    return response;
-  })
-
+ const disconnectSocket = () => {
+  console.log('Disconnecting socket...');
+  if(socket) socket.disconnect();
 }
-  const [color, setColor] = useState({ r: 0, g: 0, b: 0 });
+function App() {
+  const [leds , setLeds] = useState([]);
+ const handleLedChange=async (e)=>{
+  socket.emit("ledwall", e);
 
+
+
+ }
+
+
+
+
+  const getLeds = async () => {
+   
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+ 
+   
+  };
+  const initLeds= await fetch('http://localhost:3005/getleds',requestOptions);
+ 
+  let initialLeds= await initLeds.json();
+  setLeds(initialLeds)
+} 
+
+  useEffect(  ()=>{
+  getLeds()
+  initiateSocketConnection()
+  return()=>{
+
+   console.log('clearing')
+   
+   disconnectSocket();
+    
+  
+   
+   }
+},[]
+  
+  
+  
+  
+  )
+  console.log('leds',leds)
+ 
   return(
-  <div>
-  <RgbaColorPicker color={color} onChange={setColor} />;
-  <button onClick={handleSetColor}>Näytä Väri</button>
-  </div>
+   
+    
+    <>
+    <div id='container'></div>
+    <Stage container='container' width={500} height={500} >
+  
+      <Layer>
+       
+       
+      {leds.map((led) => (
+          <Circle
+            key={led._id}
+            id={led.num}
+            x={led.posX}
+            y={led.posY}
+            radius={2}
+            fill="#89b717"
+            opacity={0.8}      
+            shadowColor="black"
+            shadowBlur={10}
+            shadowOpacity={0.6}
+            shadowOffsetX={led.isDragging ? 10 : 5}
+            shadowOffsetY={led.isDragging ? 10 : 5}
+            scaleX={led.isDragging ? 1.2 : 1}
+            scaleY={led.isDragging ? 1.2 : 1}
+            onMouseEnter={()=>handleLedChange(led)}
+           
+           
+     
+           
+          />
+        ))}
+        
+      </Layer>
+
+    </Stage>
+   
+    
+    </>
   )
 };
 
